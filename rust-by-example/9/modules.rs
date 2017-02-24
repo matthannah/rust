9406fabd106eaa80554b5988528562b618296d80
matthannah <mryanhannah@gmail.com>
2017-02-24 20:54:34 +1100

    NEW: Start chapter 9

---
+++ rust-by-example/9/modules.rs
@@ -0,0 +1,6 @@
+// Rust provides a powerful module system that can be used to hierachically split
+// code into logical units (modules), and manage visibility (public/private) between
+// them.
+
+// A module is a collection of items: functions, structs, traits, impl, and even other
+// modules
