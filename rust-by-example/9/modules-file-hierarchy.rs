c37e00182764957f6d7155fa10085ecd6080b679
matthannah <mryanhannah@gmail.com>
2017-02-25 15:16:43 +1100

    ENHANCE: Finish chapter 9

---
+++ rust-by-example/9/modules-file-hierarchy.rs
@@ -0,0 +1,13 @@
+// Modules can be mapped to a file/directory hierarchy. lets break down the visbility
+// example in directory file-hierarchy:
+
+
+/* $ tree .
+.
+|--file-hierarchy-- my
+          |         |-- inaccessible.rs
+          |         |-- mod.rs
+          |         `-- nested.rs
+          |
+          `-- split.rs
+*/
