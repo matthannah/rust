8d1220b2bf2c47a2facee0633e23e41ce8f893f4
matthannah <mryanhannah@gmail.com>
2017-02-26 16:12:54 +1100

    NEW: Add rosetta programming tasks for rust

---
+++ rosetta/100-doors/main.rs
@@ -0,0 +1,53 @@
+#[allow(dead_code)]
+enum Door {
+    Open,
+    Closed
+}
+
+impl Door {
+    fn toggle(&mut self) {
+        match *self {
+            Door::Open => { *self = Door::Closed },
+            Door::Closed => { *self = Door::Open }
+        }
+    }
+}
+
+impl std::fmt::Display for Door {
+    fn fmt(&self, f: &mut std::fmt::Formatter) -> std::fmt::Result {
+        match *self {
+            Door::Open => write!(f, "Open"),
+            Door::Closed => write!(f, "Closed")
+        }
+    }
+}
+
+fn main() {
+    let num_of_passes: usize = 100;
+    let mut doors: Vec<Door> = initialise_doors();
+
+    for pass_number in 1 .. num_of_passes + 1 {
+        for (door_num, door) in doors.iter_mut().enumerate() {
+            if (door_num + 1) % pass_number == 0 { door.toggle() }
+        }
+    }
+
+    print_doors(&mut doors);
+}
+
+fn initialise_doors() -> Vec<Door> {
+    let num_of_doors: u8 = 100;
+    let mut doors: Vec<Door> = Vec::new();
+
+    for _ in 0 .. num_of_doors {
+        doors.push(Door::Closed);
+    }
+
+    doors
+}
+
+fn print_doors(doors: &mut Vec<Door>) {
+    for (door_num, door) in doors.iter().enumerate() {
+        println!("door number {} is {}", door_num + 1, door);
+    }
+}
---
+++ rosetta/README.md
@@ -0,0 +1,3 @@
+#Rosetta Code Programming Tasks
+
+Implementations of programming tasks in Rust. Implementations are intended as a learning tool, therefore optimizations/code size are not considered. 
