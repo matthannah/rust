5ac228f4a79ac0901190968e04365901888735d9
matthannah <mryanhannah@gmail.com>
2017-03-02 19:34:38 +1100

    ENHANCE: Add associated types

---
+++ rust-by-example/12 - Generics/Associated Items/associated-items.rs
@@ -0,0 +1,6 @@
+// "Associated Items" refers to a set of rules pertaining to items of various
+// types. It is an extension to trait generics, and allows traits to internally
+// define new items.
+
+// One such item is called an associated type, providing simpler usage patterns
+// when the trait is generic over its container type.
